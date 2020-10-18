00748a7034ff7b4e31f4ed32c5438b809d914132
Francisco Payés <payesfrancisco@gmail.com>
2020-10-18 15:47:35 +0200

    initial-commit: create project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="navbar-fixed">
+        <nav className="blue">
+          <div className="nav-wrapper">
+            <div className="row">
+              <div className="col s12">
+                <a href="#" className="brand-container">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <span>Presidentes de la República Dominicana</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }

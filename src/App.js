aae15854fe2488ca7688064b59dc6314026abd2b
Francisco Payés <payesfrancisco@gmail.com>
2020-10-31 18:07:51 +0100

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Timeline from './Components/Timeline'
+import Timeline from "./Components/Timeline";
 
 function App() {
   return (
@@ -15,7 +15,12 @@ function App() {
             <div className="row">
               <div className="col s12">
                 <a href="#" className="brand-container">
-                  <img title="See File history below for details., Public domain, via Wikimedia Commons" src={logo} className="App-logo" alt="logo" />
+                  <img
+                    title="See File history below for details., Public domain, via Wikimedia Commons"
+                    src={logo}
+                    className="App-logo"
+                    alt="logo"
+                  />
                   <span>Presidentes de la República Dominicana</span>
                 </a>
               </div>
@@ -26,6 +31,16 @@ function App() {
       <div className="content">
         <Timeline />
       </div>
+      <footer className="page-footer blue">
+        <div className="footer-copyright">
+          <div className="container">
+            2020 - Desarrollado por <a href="https://github.com/fxisco" className="plain-link">fxisco</a>
+            <a className="grey-text text-lighten-4 right" href="#!">
+              Github project
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
